b5dc9c88b2ff82c4234cd24aa6e2ba219af968e2
EnergoStalin <alex.vipers@mail.ru>
2023-01-19 01:05:08 +0300

    merge into one file add drawio

--- 19-01-2023/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ 19-01-2023/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
 #include <math.h>
-#include "tasks.hpp"
+
+// long double длинное число(64 бита) с плавающей точкой
+long double f1(double x) {
+	return (1 + pow(x, 2)) / (2 - sqrt(1 + pow(x, 4)));
+}
+
+long double f2(double x) {
+	return (1 + fabs(x)) / (pow(1 + x + pow(x, 3), 1 / 3));
+}
+
+void t1() {
+    double x[4]; // Статический(память освобождать ненужно) массив на 4 элемента
+
+    // Читаем массив в цикле
+    // size_t - длинное беззнаковое число(не может быть отрицательным)
+    for (size_t i = 0; i < 4; i++) {
+        std::cin >> x[i];
+    }
+
+    // Обычная сортировка пузырьком(bubble sort)
+    // https://en.wikipedia.org/wiki/Bubble_sort
+    for (size_t i = 0; i < 4; i++) {
+        for (size_t j = 0; j < 3; j++) {
+            if (x[j] > x[j + 1]) {
+                double t = x[j];
+                x[j] = x[j + 1];
+                x[j + 1] = t;
+            }
+        }
+    }
+
+    // Поскольку массив теперь отсортированный можно просто взять первые 2
+    // элемента или последние не помню
+    std::cout << x[0] << std::endl; // endl - конец строки
+    std::cout << x[1] << std::endl;
+}
 
 int main() {
 	std::cout << f1(81) << std::endl; // Вызов функции с 1 аргументом и вывод его в консоль
--- 19-01-2023/ConsoleApplication1/ConsoleApplication1/tasks.hpp
+++
@@ -1,7 +0,0 @@
-// Гарантирует что только один раз будет подключен этот файл
-#pragma once
-
-long double f1(double x);
-long double f2(double x);
-
-void t1();
